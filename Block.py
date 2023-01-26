45c9338926010136484bc8187e3197bec8c7c0ab
championkwsp <champ.komsan@gmail.com>
2023-01-26 21:59:47 +0700

    regenerate hash when the data is modified

--- Block.py
+++ Block.py
@@ -10,15 +10,17 @@ class Block:
             
     def set_hash_prev(self, hash_prev):
         self.hash_prev = hash_prev
+        self.hash_itself = self.generate_hash_itself()
         
     def get_transactions(self):
         return self.transactions
         
-    def set_hash_prev(self, transactions):
+    def set_transactions(self, transactions):
         self.transactions = transactions
+        self.hash_itself = self.generate_hash_itself()
         
-    def reset_hash_prev(self):
-        self.set_hash_prev([])
+    def reset_transactions(self):
+        self.set_transactions([])
         
     def generate_hash_itself(self):
         return hash(str(self.get_hash_prev()) + ','.join(self.transactions))
